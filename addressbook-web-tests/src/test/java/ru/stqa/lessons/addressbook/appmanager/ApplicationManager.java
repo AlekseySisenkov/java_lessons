71ffa04f3f7aa6910eb6b807e6fb8aa9739d9981
AlekseySisenkov <bf3err@gmail.com>
2022-06-20 17:30:21 +0300

    Лекция 2.7. Выделение специализированных классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/appmanager/ApplicationManager.java
@@ -2,55 +2,25 @@ package ru.stqa.lessons.addressbook.appmanager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.stqa.lessons.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  private WebDriver wd;
+  private NavigationHelper navigationHelper;
+  private GroupsHelper groupsHelper;
+  private SessionHelper sessionHelper;
   private JavascriptExecutor js;
+  WebDriver wd;
 
   public void init() {
     wd = new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+   wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     js = (JavascriptExecutor) wd;
     wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  public void returntoGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  public void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
-  public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    groupsHelper = new GroupsHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
   public void stop() {
@@ -75,11 +45,11 @@ public class ApplicationManager {
     }
   }
 
-  public void deleteSelectedGroup() {
-    wd.findElement(By.name("delete")).click();
+  public GroupsHelper getGroupsHelper() {
+    return groupsHelper;
   }
 
-  public void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/GroupDeletionTests.java
@@ -4,10 +4,10 @@ import org.testng.annotations.Test;
 public class GroupDeletionTests extends TestBase{
   @Test
   public void testGroupDeletion() throws Exception {
-    app.gotoGroupPage();
-    app.selectGroup();
-    app.deleteSelectedGroup();
-app.returntoGroupPage();
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupsHelper().selectGroup();
+    app.getGroupsHelper().deleteSelectedGroup();
+app.getGroupsHelper().returntoGroupPage();
   }
 
 }
