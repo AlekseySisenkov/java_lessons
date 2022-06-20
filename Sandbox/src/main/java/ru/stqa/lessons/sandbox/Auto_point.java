f23447b5947463a03bdc4d2633d3bacd7865a415
AlekseySisenkov <bf3err@gmail.com>
2022-06-20 07:12:09 +0300

    Задание №3

--- Sandbox/src/main/java/ru/stqa/lessons/sandbox/Auto_point.java
+++ Sandbox/src/main/java/ru/stqa/lessons/sandbox/Auto_point.java
@@ -8,9 +8,13 @@ public class Auto_point {
     Point p2 = new Point(-6,5);
     System.out.println("Расстояние между точками p1 и p2 = " + distance(p1, p2));
 
-    Point p3 = new Point(1,-15);
-    Point p4 = new Point(-6,5);
+    Point p3 = new Point(-1,4);
+    Point p4 = new Point(-4,9);
     System.out.println("Расстояние между точками p3 и p4 = " + p3.distance(p4));
+
+    Point p5 = new Point(34,2);
+    Point p6 = new Point(-10,-6);
+    System.out.println("Расстояние между точками p3 и p4 = " + p5.distance(p6));
   }
 
   public static double distance(Point p1, Point p2) {
---
+++ Sandbox/src/test/java/ru/stqa/lessons/sandbox/Point_tests.java
@@ -0,0 +1,33 @@
+package ru.stqa.lessons.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Point_tests {
+
+
+  @Test
+  public void testArea1 () {
+
+    Point p1 = new Point(1, -15);
+    Point p2 = new Point(-6, 5);
+
+    assert p1.distance(p2) == 21.18962010041709;
+  }
+
+  @Test
+  public void testArea2 () {
+
+    Point p3 = new Point(-1, 4);
+    Point p4 = new Point(-4, 9);
+    Assert.assertEquals(p3.distance(p4), -5.830951894845301);
+  }
+
+  @Test
+  public void testArea3 () {
+
+    Point p5 = new Point(34,2);
+    Point p6 = new Point(-10,-6);
+    Assert.assertEquals(p5.distance(p6),44.721359549995796);
+  }
+}
