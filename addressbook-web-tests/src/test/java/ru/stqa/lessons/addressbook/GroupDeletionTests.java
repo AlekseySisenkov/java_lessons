ab82e6024f17b65bb673171ecca8680d9520ffac
AlekseySisenkov <bf3err@gmail.com>
2022-06-20 16:46:14 +0300

    Лекция 2.5. Создание общего базового класса для тестов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/GroupDeletionTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.lessons.addressbook;
+
+import org.testng.annotations.Test;
+public class GroupDeletionTests extends TestBase{
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroup();
+returntoGroupPage();
+  }
+
+}
