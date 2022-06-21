fd05ad4431c84ea112a5003f1ec3533c953ef1bb
AlekseySisenkov <bf3err@gmail.com>
2022-06-21 08:50:57 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/ContactDeletionTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class ContactDeletionTests extends TestBase {
   @Test
   public void testContactDeletion(){
-   app.getContactHelper().selecteContact(8);
+   app.getContactHelper().selecteContact(13);
    app.getContactHelper().deleteSelectedContact();
    app.getContactHelper().allert();
   }
