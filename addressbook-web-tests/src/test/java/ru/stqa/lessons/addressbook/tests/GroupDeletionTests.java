d20eceee5b5ccf45096bb1a8d8001b8cf24e3ee5
AlekseySisenkov <bf3err@gmail.com>
2022-06-23 13:52:57 +0300

    Лекция 4.6. Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/GroupDeletionTests.java
@@ -20,6 +20,8 @@ public class GroupDeletionTests extends TestBase{
     List<GroupData> after = app.getGroupsHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size()-1);
 
+    before.remove(before.size()-1);
+      Assert.assertEquals(after,before);
   }
 
 }
