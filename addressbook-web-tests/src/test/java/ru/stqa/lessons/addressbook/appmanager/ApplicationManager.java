528e633dc3fe077201d6cd1ef5b83ed3dea23dce
AlekseySisenkov <bf3err@gmail.com>
2022-06-26 12:14:32 +0300

    Лекция 7.4. Сравнение данных, загруженных из БД

--- addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
   private JavascriptExecutor js;
   WebDriver wd;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
 
@@ -31,6 +32,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target","local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+    dbHelper = new DbHelper();
 
     if (browser.equals(BrowserType.CHROME)){
       wd = new ChromeDriver();
@@ -62,4 +65,6 @@ public class ApplicationManager {
   public ContactHelper contact() {
     return contactHelper;
   }
+
+  public DbHelper db(){return dbHelper;}
 }
--- addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/model/GroupData.java
@@ -28,19 +28,6 @@ public class GroupData {
   @Column(name = "group_footer")
   private String footer;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id && Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
   public String getName() {
     return name;
   }
@@ -76,6 +63,18 @@ public class GroupData {
     return this;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id && Objects.equals(name, groupData.name) && Objects.equals(header, groupData.header) && Objects.equals(footer, groupData.footer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, header, footer);
+  }
   @Override
   public String toString() {
     return "GroupData{" +
