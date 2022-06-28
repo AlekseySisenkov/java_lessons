f35a2a3e10e3fd9a7e3b1bb5dd0e941a4192f606
AlekseySisenkov <bf3err@gmail.com>
2022-06-29 00:38:26 +0300

    Доработка Задания №11

--- addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/ContactInfoHomeVsInfoEditTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lessons/addressbook/tests/ContactInfoHomeVsInfoEditTests.java
@@ -12,43 +12,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactInfoHomeVsInfoEditTests extends TestBase{
 
   @Test
-  public void testContactAddress() {
+  public void testContactInfoHomeVsInfoEdit() {
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditFom(contact);
 
     assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
-  }
-
-
-  @Test
-  public void testContactPhones(){
-    ContactData contact = app.contact().all().iterator().next();
-    ContactData contactInfoFromEditForm = app.contact().infoFromEditFom(contact);
-
     assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
-  }
+    assertThat(contact.getAllEMail(), equalTo(mergeEMails(contactInfoFromEditForm)));
 
+  }
   private <T> String mergePhones(ContactData contact) {
     return Arrays.asList(contact.getHomep(),contact.getMobilep(),contact.getWorkp(),contact.getSecondaryPhone())
             .stream().filter((s) -> !s.equals(""))
             .map(ContactInfoHomeVsInfoEditTests::cleaned).collect(Collectors.joining("\n"));
   }
 
-  public static String cleaned(String phone){
-    return phone.replaceAll("\\s","").replaceAll("[-()]","");
-  }
-
-
-  @Test
-  public void testContactEMail() {
-    ContactData contact = app.contact().all().iterator().next();
-    ContactData contactInfoFromEditForm = app.contact().infoFromEditFom(contact);
-
-    assertThat(contact.getAllEMail(), equalTo(mergeEMails(contactInfoFromEditForm)));
-  }
-
   private <T> String mergeEMails(ContactData contact) {
     return Arrays.asList(contact.getEmail(),contact.getEmail2(),contact.getEmail3())
             .stream().filter((s) -> !s.equals("")).collect(Collectors.joining("\n"));
+  }
+  public static String cleaned(String phone){
+    return phone.replaceAll("\\s","").replaceAll("[-()]","");
   }
 }
